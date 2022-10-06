7bcd86a3184c4dea048f7f62467c1fe39e38dd78
Akshay Dubey <akshay.dubey055@gmail.com>
2022-10-06 21:56:08 +0530

    Initial commit

---
+++ src/main/java/com/github/itsAkshayDubey/aopwithspringpart1/AopWithSpringPart1Application.java
@@ -0,0 +1,13 @@
+package com.github.itsAkshayDubey.aopwithspringpart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopWithSpringPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopWithSpringPart1Application.class, args);
+	}
+
+}
