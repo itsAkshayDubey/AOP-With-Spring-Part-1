74e99c5678c87d203d39807ead4b73f8c8247fee
Akshay Dubey <akshay.dubey055@gmail.com>
2022-10-07 00:01:46 +0530

    Add throws in service interface

--- src/main/java/com/github/itsAkshayDubey/aopwithspringpart1/service/intf/EmployeeServiceInterface.java
+++ src/main/java/com/github/itsAkshayDubey/aopwithspringpart1/service/intf/EmployeeServiceInterface.java
@@ -2,9 +2,10 @@ package com.github.itsAkshayDubey.aopwithspringpart1.service.intf;
 
 import java.util.Optional;
 
+import com.github.itsAkshayDubey.aopwithspringpart1.exception.EmployeeNotFoundException;
 import com.github.itsAkshayDubey.aopwithspringpart1.model.Employee;
 
 public interface EmployeeServiceInterface {
-	Optional<Employee> getEmployeeById(Integer id);
+	Optional<Employee> getEmployeeById(Integer id) throws EmployeeNotFoundException;
 	Employee addEmployee(Employee employee);
 }
