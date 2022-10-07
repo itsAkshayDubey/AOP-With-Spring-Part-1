222eafce305e233117aec8ca5f60445aaad8401b
Akshay Dubey <akshay.dubey055@gmail.com>
2022-10-07 11:11:58 +0530

    Add global exception handler

---
+++ src/main/java/com/github/itsAkshayDubey/aopwithspringpart1/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.github.itsAkshayDubey.aopwithspringpart1.exception.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.github.itsAkshayDubey.aopwithspringpart1.error.ErrorDetails;
+import com.github.itsAkshayDubey.aopwithspringpart1.exception.EmployeeNotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(EmployeeNotFoundException ex, WebRequest request) {
+         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
